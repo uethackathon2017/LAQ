ac3ee265d80b3fd5bd3883df9235c2daae974639
Machao <machaomachaom1@gmail.com>
2017-03-10 22:06:29 +0700

    Menu Activity

---
+++ app/src/main/java/com/laqa/fastestenglish/MenuActivity.java
@@ -0,0 +1,69 @@
+package com.laqa.fastestenglish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MenuActivity extends AppCompatActivity implements View.OnTouchListener,View.OnClickListener{
+
+    ImageButton menuButtonPlay, menuButtonPacks, menuButtonRecord,menuButtonSetting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        menuButtonPlay=(ImageButton)findViewById(R.id.menuButtonPlay);
+        menuButtonPacks=(ImageButton)findViewById(R.id.menuButtonPacks);
+        menuButtonRecord=(ImageButton)findViewById(R.id.menuButtonRecord);
+        menuButtonSetting=(ImageButton)findViewById(R.id.menuButtonSetting);
+
+        menuButtonPlay.setOnTouchListener(this);
+        menuButtonPacks.setOnTouchListener(this);
+        menuButtonRecord.setOnTouchListener(this);
+        menuButtonSetting.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if(view.getId()== R.id.menuButtonPlay ||
+            view.getId()== R.id.menuButtonPacks ||
+            view.getId()== R.id.menuButtonRecord ||
+            view.getId()== R.id.menuButtonSetting){
+
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                view.animate().setDuration(100);
+                view.animate().scaleX(1.2f);
+                view.animate().scaleY(1.2f);
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                view.animate().setDuration(100);
+                view.animate().scaleX(1.0f);
+                view.animate().scaleY(1.0f);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.menuButtonPlay:
+                //Chuyen sang trang Play
+                break;
+
+            case R.id.menuButtonPacks:
+                //Chuyen sang trang Packs
+                break;
+
+            case R.id.menuButtonRecord:
+                //Chuyen sang trang Record
+                break;
+
+            case R.id.menuButtonSetting:
+                //Goi Fragment Setting
+                break;
+        }
+    }
+}
